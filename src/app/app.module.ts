f0b057a0485b99b7c7115f40075001731c386325
Alan Henrique <alan_hac@outlook.com>
2019-09-28 23:33:46 -0300

    Commit inicial dados fixos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './components/info/info.component';
+import { AbilitiesComponent } from './components/abilities/abilities.component';
+import { ExpirienceComponent } from './components/expirience/expirience.component';
+import { AcademyComponent } from './components/academy/academy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    AbilitiesComponent,
+    ExpirienceComponent,
+    AcademyComponent
   ],
   imports: [
     BrowserModule
